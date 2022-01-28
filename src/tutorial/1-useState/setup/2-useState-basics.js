a435a5e583b422f2c8d80acfcfafec6d70bd792f
mubashir-cvr <mubashircheruvayoor@gmail.com>
2022-01-28 20:12:06 +0530

    Call  use state function to change text

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  const[text,setText]=useState('Random Text')
+  const[text,setText]=useState('Default Text')
+  const handleClick=()=>{
+    setText("Changed Text")
+  }
   return <React.Fragment>
     <h2>{text}</h2>
+    <button className='btn' onClick={handleClick}>My Button</button>
   </React.Fragment>;
 };
 
