8707fdebf91348899e4b631bea81a90835446290
mubashir-cvr <mubashircheruvayoor@gmail.com>
2022-01-28 20:15:23 +0530

    Use state effect

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -3,7 +3,10 @@ import React, { useState } from 'react';
 const UseStateBasics = () => {
   const[text,setText]=useState('Default Text')
   const handleClick=()=>{
+    if(text==='Default Text')
     setText("Changed Text")
+    else
+    setText("Default Text")
   }
   return <React.Fragment>
     <h2>{text}</h2>
