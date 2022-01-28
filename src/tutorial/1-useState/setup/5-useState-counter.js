63a40e28212b5c8eed992da5c98b189bdaece0c9
mubashir-cvr <mubashircheruvayoor@gmail.com>
2022-01-28 23:51:10 +0530

    sync and Async using usestate function

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,37 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value,setValue]=useState(0)
+  const complexIncrease=()=>{
+    setTimeout(()=>{
+      // setValue(value+1); This will update the value with one even we are clicking multiple times in 2 sec
+      
+      setValue((prevValue)=>{
+        return prevValue+1;
+
+      })
+      //prevValue is previous value functional approch is async
+
+    },2000);
+
+  }
+  return <>
+  <section style={{margin:'4rem 0'}}>
+    <h2>Regular Counter</h2>
+    <h1>{value}</h1>
+    <button className='btn' onClick={()=>setValue(value-1)}>Dicrease</button>
+    <button className='btn'onClick={()=>setValue(0)}>RESET</button>
+    <button className='btn' onClick={()=>setValue(value+1)}>Increase</button>
+
+  </section>
+
+   <section style={{margin:'4rem 0'}}>
+    <h2>Regular Counter</h2>
+    <h1>{value}</h1>
+    <button className='btn' onClick={complexIncrease}> Increase Later</button>
+    
+  </section>
+  </>;
 };
 
 export default UseStateCounter;
