3e6cded8d4fa88699566d578ee05c456cafa6400
mubashir-cvr <mubashircheruvayoor@gmail.com>
2022-01-28 21:03:46 +0530

    Split Object

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -4,16 +4,21 @@ const UseStateObject = () => {
   const [person,setPeople]=useState({
     name:"Mubashir",
     age:23,
-    message:"My Custom Message"
-  })
+    message:"My Custom Message",
+  });
+  const [name,setName]=useState('Mubashir');
+  const [age,setAge]=useState(23);
+  const [message,setMessage]=useState('My Custom Message');
   const changeMessage=()=>{
-    setPeople({...person,message:'Hello World'})
+    // setPeople({...person,message:'Hello World'})
+    
+    setMessage('Hello World');
   }
   return <>
-  <h3>{person.name}</h3>
-  <h3>{person.age}</h3>
-  <h3>{person.message}</h3>
-  <button className='btn' onClick={changeMessage}>CHange message</button>
+  <h3>{name}</h3>
+  <h3>{age}</h3>
+  <h3>{message}</h3>
+  <button className='btn' onClick={changeMessage}>Change message</button>
   </> ;
 };
 
