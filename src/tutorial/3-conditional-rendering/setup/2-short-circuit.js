dbd8c152b9aafe8eb4b5dd5515b78f97ba868971
mubashir-cvr <mubashircheruvayoor@gmail.com>
2022-01-29 16:31:09 +0530

    ?: ternory operatior

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -5,6 +5,7 @@ import React, { useState } from 'react';
 const ShortCircuit = () => {
   const [text,setText]=useState('Hi')
   const [text1, setText1] = useState("");
+  const [error, setError] = useState(true);
 
   return (
     <>
@@ -14,6 +15,7 @@ const ShortCircuit = () => {
       {text1 && <h4>Text is contain a value</h4>}
       <h6>------------------------------------</h6>
       {!text1 && <h4>Text is contain a value</h4>}
+      {error ? (<h4>Text is contain a Error</h4>):(<h4>No error</h4>)}
     </>
   );
 };
