1ffa99c96249e586b0565daafcd814fd49486783
mubashir-cvr <mubashircheruvayoor@gmail.com>
2022-01-29 10:12:41 +0530

    Clean Up UseEffect - Desable eventListener once rendered

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return ()=>{
+      console.log('Clean Up');
+      window.removeEventListener('resize',checkSize)
+    }
+  });
+
+  const [size, setSize] = useState(window.innerWidth);
+  return (
+    <>
+      <h2>Window</h2>
+      <h3>{size} PX</h3>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
