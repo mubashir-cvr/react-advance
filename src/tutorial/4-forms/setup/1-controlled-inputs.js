10e6c18adda678475966ab4a155f090742ee8237
mubashir-cvr <mubashircheruvayoor@gmail.com>
2022-01-29 23:55:08 +0530

    Controlled input

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -8,8 +8,20 @@ import React, { useState } from "react";
 const ControlledInputs = () => {
   const [firstName,setFirstName]=useState('')
   const [email, setEmail] = useState('');
+  const [people, setPeople] = useState([])
   const handleSubmit= (e)=>{
     e.preventDefault()
+    if(firstName && email){
+      const person={
+        id:new Date().getTime().toString(),
+        firstName,email
+      }
+      setPeople(()=>{
+        return [...people,person];
+      })
+      setEmail("");
+      setFirstName("");
+    }
     console.log("Form Submited")
 
   }
@@ -44,6 +56,15 @@ const ControlledInputs = () => {
             Add Person
           </button>
         </form>
+        {
+          people.map((person)=>{
+            const {id,firstName,email}=person
+            return <div className="item" key={id}>
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          })
+        }
       </article>
     </>
   );
