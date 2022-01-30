b75dbbe439fa32aa8918a873c258ef1e486e3cf1
mubashir-cvr <mubashircheruvayoor@gmail.com>
2022-01-30 12:04:43 +0530

    Using different component from index.js

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,48 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name, setName] = useState("");
+  const [people, setPeople] = useState(data);
+  const [showModel, setShowModel] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+      setPeople([...people,{id:new Date().getTime().toString(),name}])
+      setName('')
+      setShowModel(false);
+    } else {
+      setShowModel(true);
+    }
+  };
+  return (
+    <>
+      {showModel && <Modal />}
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+          <input
+            type="text"
+            name="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+         />
+          <button type="submit">Add</button>
+        </div>
+      </form>
+      {
+        people.map((person)=>{
+          return <div>
+            <h4>{person.name}</h4>
+
+          </div>
+        })
+      }
+    </>
+  );
 };
 
 export default Index;
