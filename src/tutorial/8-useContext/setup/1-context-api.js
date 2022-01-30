87f856e63513821413fa73b1207fd8986fb297ee
mubashir-cvr <mubashircheruvayoor@gmail.com>
2022-01-30 23:53:00 +0530

    Passing contect to child div

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react';
 import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
-
+const PersonContext = React.createContext();
 const ContextAPI = () => {
   const [people, setPeople] = useState(data);
   const removePerson = (id) => {
@@ -11,10 +11,10 @@ const ContextAPI = () => {
     });
   };
   return (
-    <>
+    <PersonContext.Provider value="hello">
       <h3>prop drilling</h3>
       <List people={people} removePerson={removePerson} />
-    </>
+    </PersonContext.Provider>
   );
 };
 
@@ -35,6 +35,8 @@ const List = ({ people, removePerson }) => {
 };
 
 const SinglePerson = ({ id, name, removePerson }) => {
+  const data= useContext(PersonContext)
+  console.log(data)
   return (
     <div className='item'>
       <h4>{name}</h4>
